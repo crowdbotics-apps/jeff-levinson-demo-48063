838747b831ed22ea2fefaf936ede6cb17564d765
Crowdbotics <team@crowdbotics.com>
2024-05-22 18:35:39 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "jeff-levinson-demo-48063",
+  url: "https://jeff-levinson-demo-48063.botics.co",
+  api: "https://jeff-levinson-demo-48063.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
